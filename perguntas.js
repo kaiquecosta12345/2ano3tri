9d174afc0c0960635209bdc68d7286993ffc04ef
kaiquecosta12345 <163871330+kaiquecosta12345@users.noreply.github.com>
2024-10-21 10:39:52 -0300

    jogadores bom igual o cr7

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'copa do mundo',
+    'qual foi a ultima copa que o ronaldo fenomeno ganho?',
+    'Copa do Mundo de 2002  (RAI)'
+)
